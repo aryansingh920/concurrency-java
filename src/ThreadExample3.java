d705fac7bdde3da074fa9ec167de33e6d8021590
Aryan Singh <aryansingh920@outlook.com>
2026-02-19 20:23:26 +0530

    Fix formatting in comments for clarity in ThreadExample3 class

--- src/ThreadExample3.java
+++ src/ThreadExample3.java
@@ -1,8 +1,9 @@
 public class ThreadExample3 {
 
     /*
-     Third method is to implement the Runnable interface
-     as an anonymous class
+     * Third method is to implement the Runnable interface
+     * as an anonymous class
+     * Fourth way is to do the same with lambda expression
      */
 
     public static void main(String[] args) {
